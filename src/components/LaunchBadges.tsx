d3bf9470cf8e28a20cdf5722bbc1d38198b9c4f4
assetforge-labs <assetforgelabs@gmail.com>
2026-05-23 19:11:52 +0530

    Update LaunchBadges.tsx

--- src/components/LaunchBadges.tsx
+++ src/components/LaunchBadges.tsx
@@ -3,7 +3,7 @@ export default function LaunchBadges() {
     <div className="flex flex-col sm:flex-row items-center justify-center gap-6 mt-12 mb-8">
       
       {/* 1. PASTE YOUR CONVERTED PRODUCT HUNT CODE EXACTLY BELOW THIS LINE */}
-      <a href="https://www.producthunt.com/products/assetforge-labs?embed=true&amp;utm_source=badge-featured&amp;utm_medium=badge&amp;utm_campaign=badge-assetforge-labs" target="_blank" rel="noopener noreferrer"><img alt="AssetForge Labs - World's First Digital asset packaging tool | Product Hunt" width="250" height="54" src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1153627&amp;theme=light&amp;t=1779541569851"></a>
+      <a href="https://www.producthunt.com/products/assetforge-labs?embed=true&amp;utm_source=badge-featured&amp;utm_medium=badge&amp;utm_campaign=badge-assetforge-labs" target="_blank" rel="noopener noreferrer"><img alt="AssetForge Labs - World's First Digital asset packaging tool | Product Hunt" width="250" height="54" src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1153627&amp;theme=light&amp;t=1779541569851" /></a>
       
     
       {/* 2. PASTE YOUR CONVERTED SHIPIT CODE EXACTLY BELOW THIS LINE */}
